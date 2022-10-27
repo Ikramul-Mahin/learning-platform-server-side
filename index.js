8ce0d292f3d27406a8ce99c816dd18f8b1c1d14f
Ikramul-Mahin <ikramul.mahin.islam@gmail.com>
2022-10-27 11:02:27 +0800

    added

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000;
+app.use(cors())
+
+const categories = require('./data/categories.json')
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id
+    const categoryCourse = courses.find(n => n.category_id === id)
+    res.send(categoryCourse)
+})
+
+
+const courses = require('./data/courses.json')
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id
+    const selectedCourse = courses.find(c => c.category_id === id)
+    res.send(selectedCourse)
+
+})
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
